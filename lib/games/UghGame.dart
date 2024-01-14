4956de2812e1c13e35d086b5e04f6a3f77e07cf0
cxrlitxs <carloscastanedo.88@gmail.com>
2024-01-14 18:36:28 +0100

    Map Tiles working

--- lib/games/UghGame.dart
+++ lib/games/UghGame.dart
@@ -1,15 +1,18 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flame/camera.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import '../players/EmberPlayer.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 
 class UghGame extends FlameGame{
 
   final world = World();
   late final CameraComponent cameraComponent;
   late EmberPlayer _player;
+  late TiledComponent mapComponent;
 
   @override
   Future<void> onLoad() async {
@@ -30,10 +33,19 @@ class UghGame extends FlameGame{
     cameraComponent.viewfinder.anchor = Anchor.topLeft;
     addAll([cameraComponent, world]);
 
+    mapComponent=await TiledComponent.load('mapa1.tmx', Vector2.all(32));
+    world.add(mapComponent);
+
     _player = EmberPlayer(
       position: Vector2(128, canvasSize.y - 150),
     );
     world.add(_player);
+  }
+
+  @override
+  Color backgroundColor() {
+    // TODO: implement backgroundColor
+    return Color.fromRGBO(43, 6, 77, 1.0);
   }
 
 }
